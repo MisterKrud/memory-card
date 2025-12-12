fdeddf3a73dea672cb3bbe23115b7bdb7e3b8b5f
MisterKrud <162855452+MisterKrud@users.noreply.github.com>
2025-12-12 23:05:30 +1100

    Refactor 'isTouched' -now handled by a parent array

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,32 +1,16 @@
-import { useEffect, useState } from "react";
-
-export function Card({imgKey, imgSrc, onScore, zeroScore, checkGlobal}){
-
-const [isTouched, setIsTouched] = useState(false)
-//   const isCardTouched = globalTouchedState ? true : isTouched
-
-useEffect(()=>{
-   checkGlobal === 1 ? setIsTouched(false) : null
-   
 
 
-},[checkGlobal])
+export function Card({ imgKey, imgSrc, isTouched, handleClick }) {
 
-  function handleTouched(){
-   if(!isTouched){
-    setIsTouched(true);
-    onScore()
-} else {
-setIsTouched(true)
-    zeroScore()
-}
-  }
-
-
-  return(
-    <div key={imgKey} className = {'card'+' '+'touched-'+isTouched}  >
-      <img className="emoji" key={imgKey} src={imgSrc} touched={isTouched} onClick={handleTouched} />
-    
+  return (
+    <div key={imgKey} onClick={handleClick} className={"card" + " " + "touched-" + isTouched}>
+      <img
+        className="emoji"
+        key={imgKey}
+        src={imgSrc}
+        touched={isTouched}
+        
+      />
     </div>
-  )
+  );
 }
--- src/components/CardTable.jsx
+++ src/components/CardTable.jsx
@@ -1,19 +1,18 @@
 import { useState, useEffect } from "react";
 import { Card } from "./Card";
 
-
 export default function CardTable() {
   const [data, setData] = useState(null);
+  const [touchedArray, setTouchedArray] =useState([])
   const [score, setScore] = useState(0);
   const [cards, setCards] = useState([]);
-  const [highScores, setHighScores] = useState([0])
-  const [globalTouchedIndex, setGlobalTouchedIndex] = useState(0)
+  const [highScores, setHighScores] = useState([0]);
 
 
   useEffect(() => {
     const fetchData = async () => {
       const response = await fetch(
-        "https://api.giphy.com/v2/emoji?api_key=VzJfRtnEENBiPs7cBI16fXcQmZIRuqic&limit=12&offset=21"
+        "https://api.giphy.com/v2/emoji?api_key=VzJfRtnEENBiPs7cBI16fXcQmZIRuqic&limit=24&offset=21"
       );
       if (!response.ok) {
         throw new Error(`HTTP error! status: ${response.status}`);
@@ -22,7 +21,7 @@ export default function CardTable() {
       setData(result);
     };
     fetchData();
-    console.table(data)
+    console.table(data);
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
@@ -32,69 +31,76 @@ export default function CardTable() {
         return { key: card.id, url: card.images.original_still.url };
       });
       setCards(cardArray);
-
     }
   }, [data]);
 
-
-  useEffect(() =>{
-    let tempArray = [...cards]
-    for (let i = tempArray.length -1; i>0; i--){
-      let r = Math.floor(Math.random() * (i+1));
+  useEffect(() => {
+    let tempArray = [...cards];
+    for (let i = tempArray.length - 1; i > 0; i--) {
+      let r = Math.floor(Math.random() * (i + 1));
       let n = tempArray[i];
       tempArray[i] = tempArray[r];
       tempArray[r] = n;
       setCards(tempArray);
-      console.log(cards)
+    
     }
-      // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [score])
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [score]);
 
-useEffect(()=>{
-  setGlobalTouchedIndex(0)
-},[cards])
- 
 
-  function scoreHandler(){
-    
-    setScore(s =>s+1);
+
+  function scoreHandler() {
+    setScore((s) => s + 1);
   }
 
-  function zeroScoreSetter(){
-   getHighScore()
+  function zeroScoreSetter() {
+    getHighScore();
     setScore(0);
-   setGlobalTouchedIndex(1)
 
-   
   }
 
-  function getHighScore(){
-const newHighScoreArray  = [...highScores, score]
-   setHighScores(newHighScoreArray.sort(function(a,b){return b-a}))
-    
-    console.log(`high score: ${highScores}`)
+  function getHighScore() {
+    const newHighScoreArray = [...highScores, score];
+    setHighScores(
+      newHighScoreArray.sort(function (a, b) {
+        return b - a;
+      })
+    );
+    console.log(`high score: ${highScores}`);
   }
 
-  
+   function handleClick(id){
+    console.log(`lick`)
+    console.table(touchedArray)
+   if(touchedArray.includes(id)){
+    zeroScoreSetter();
+    setTouchedArray([])
+   } else {
+    setTouchedArray([...touchedArray, id])
+    scoreHandler()
+   }
+    }
 
   return cards ? (
     <>
       <div className="card-table">
         {cards.map((i) => {
           return (
-            <Card key={i.key}
+            <Card
+              key={i.key}
               imgKey={i.key}
-              touched={false}
-              zeroScore={zeroScoreSetter}
-              onScore={scoreHandler}
+           
               imgSrc={i.url}
-              checkGlobal={globalTouchedIndex}
-              // globalTouchedState = {globalTouchedState}
+              handleClick={() => handleClick(i.key)}
+              isTouched={touchedArray.includes(i.key) ? true : false}
+         
             />
           );
         })}
       </div>
-      <div>Score: {score} Highest score: {highScores[0]}</div>
+      <div>
+        Score: {score} Highest score: {highScores[0]}
+      </div>
     </>
   ) : (
     <></>
